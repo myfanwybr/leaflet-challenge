62b296f8b475e98976dfdf46b7eac8ae228e573b
myfanwybr <m.brownrobinson@hotmail.com>
2021-02-27 10:47:11 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,12 @@
-var mymap = L.map('mapid').setView([33.7218, -100.6661], 4);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-}).addTo(mymap);
-
-// Store our API endpoint inside queryUrl
 var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"
-
+d3.json(url, function(data){
+    console.log(data)})
 
 d3.json(url, function(data){
-    
-    console.log(data)
-    var feature=data.features
+    createFeatures(data.features)
+});
 
-    function getColor(d) {
+ function getColor(d) {
         return d >90 ? '#d53e4f' :
                d >70  ? '#fc8d59' :
                d >50  ? '#fee08b' :
@@ -28,34 +16,79 @@ d3.json(url, function(data){
                '#FFEDA0';
     }
 
-    feature.forEach(element=> {
-        var coordinates= element.geometry.coordinates
-        var magnitude= element.properties.mag
-        console.log([coordinates[2]])
+function createFeatures(earthquakeData){
+   
+    var earthquakes = L.geoJSON(earthquakeData, {
+        pointToLayer: function (feature, latlng) {
+            var geojsonMarkerOptions = {
+                radius: feature.properties.mag*20000,
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                color: "white",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+        
+                return L.circle(latlng, geojsonMarkerOptions).bindPopup("<h1>" + feature.properties.place + "</h1> <hr> <h3>Magnitude: " + feature.properties.mag + "</h3> <h3>Depth: " +
+                feature.geometry.coordinates[2] + "</h3> <h3>Time: "
+                + new Date(feature.properties.time) + "</h3");    
+            }  
+    })
+    createMap(earthquakes);
+}
 
-        L.circle([coordinates[1], coordinates[0]], {
-            color: getColor(coordinates[2]),
-            // fillColor: "blue",
-            // // Adjust radius
-            radius: magnitude* 20000
-            }).bindPopup("<h1>" + element.properties.place + "</h1> <hr> <h3>Magnitude: " + magnitude + "</h3> <h3>Depth: " + coordinates[2] + "</h3> <h3>Date: "
-            + new Date(element.properties.time) + "</h3").addTo(mymap)
+function createMap(earthquakes) {
 
-    
-    })  
+    // Define streetmap and darkmap layers
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+    });
+
+    var overlayMaps = {
+        Earthquakes: earthquakes
+      };
+
+      var myMap = L.map("mapid", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [streetmap, earthquakes]
+      });
+      
     var legend = L.control({position: 'bottomright'});
   
     legend.onAdd = function (mymap) {
         var div = L.DomUtil.create('div', 'info legend'),
-            grades = [-10, 10, 30, 50, 70, 90, 10000],
-            labels = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
+            grades = [-10, 10, 30, 50, 70, 90],
+            labels = ['-10-10','10-30','30-50','50-70','70-90','90+'];
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                '<i style="background:' + getColor(grades[i] + 1) +'"></i>' +
                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
         }
         return div;
     };
-    legend.addTo(mymap);
-})
+    legend.addTo(myMap);
+
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>Earthquakes from last 7 days</h4>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+            : 'Click a bubble');
+    };
+    info.addTo(myMap)
+    
+}  
